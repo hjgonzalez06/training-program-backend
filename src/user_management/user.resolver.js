f139f30f80f67101c38d7a300add383381ad92f5
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-03-09 10:52:15 -0400

    Work on User resolvers

--- src/user_management/user.resolver.js
+++ src/user_management/user.resolver.js
@@ -1,9 +1,36 @@
+import { AuthenticationError } from 'apollo-server-express'
+
 import models from '~/src/service_providers/sequelize/models'
 
 export default {
+  Query: {
+    users: (_,args) => {
+      return models.user.findAll()
+    },
+    currentUser: {
+
+    },
+    logInUser: async (_,{email, password}) => {
+      const user = await models.user.findOne({
+        where: {
+          email
+        }
+      }) 
+      if (user && await user.validatePass(password)){
+        return user
+      }
+      throw new AuthenticationError('Invalid credentials')
+    }
+  },
   Mutation: {
-    createUser: () => {
-      return models.user.create()
+    createUser: async (_,{input}) => {
+      const response = await models.user.create({
+        firstName: input.firstName,
+        lastName: input.lastName,
+        email: input.email,
+        password: input.password
+      })
+      return response
     }
   }
 }
