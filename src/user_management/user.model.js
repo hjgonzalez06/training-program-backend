82a85466df80efec84ee3f42f5c9e438d4789d0f
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-03-09 10:22:31 -0400

    Implement User model

--- src/user_management/user.model.js
+++ src/user_management/user.model.js
@@ -1,11 +1,40 @@
+import bcrypt from 'bcrypt'
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
     }
   })
+
+  User.addHook('beforeCreate', async (user, options) => {
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
+  })
+
+  User.prototype.validatePass = async function (password) {
+    return await bcrypt.compare(password, this.password)
+  }
 
   return User
 }
